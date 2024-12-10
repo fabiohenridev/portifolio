61f74180feeb09823b9e1b8ae4f3b3d8ca735ff1
fabio henrique <henri8274@gmail.com>
2024-12-10 16:08:31 -0300

    UPDATE

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,12 +3,8 @@ import './Footer.css'
 export default function Footer(){
     return(
         <div className="BoxFooter">
-           <div className='TextPort'>
-              <h2>Trabalho portifolio Wendel</h2>
-              <i class="bi bi-person-workspace icone" ></i>
-           </div>
-           <h5>FEITO POR FABIO HENRIQUE</h5>
-           <h6>Matrícula: 01730423</h6>
+          
+            
         </div>
     )
 }
